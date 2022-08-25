08d852f00b31da1f90f41ffef0e2870276c09b26
CoolRy <that007guy@hotmail.com>
2022-08-25 11:13:54 -0400

    profile functionality up and running

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -10,6 +10,88 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .populate('friends')
+  .then((profile) => {
+    Profile.findById(req.user.profile)
+    .then(userProfile => {
+      res.render("profiles/show", {
+        title: `${profile.name}'s profile`,
+        profile,
+        userProfile,
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+function edit(req,res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    res.render('profiles/edit', {
+      title: `Editing ${profile.name}'s profile`,
+      profile
+    })
+  })
+  .catch(err =>{
+    console.log(err)
+    res.redirect('/')
+  })
+
+}
+
+function update(req, res) {
+  Profile.findOneAndUpdate(req.params.id, req.body)
+  .then((profile) => {
+    res.redirect(`/profiles/${profile._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function addFriend(req,res){
+  Profile.findById(req.user.profile)
+  .then(profile => {
+    profile.friends.push(req.params.id)
+    profile.save()
+    .then(()=> {
+    res.redirect(`/profiles/${req.params.id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function removeFriend(req,res){
+  Profile.findById(req.user.profile)
+  .then(profile => {
+    profile.friends.remove({_id: req.params.id})
+    profile.save()
+    .then (()=> {
+      res.redirect(`/profiles/${req.params.id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+
+}
+
+
 export {
-  index
+  index,
+  show,
+  edit,
+  update,
+  addFriend,
+  removeFriend
 }
